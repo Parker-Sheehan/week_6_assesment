b001eb18e61a5da9983d200f0de2c11561fc4d54
Parker Sheehan <Parkersheehan007@gmail.com>
2023-03-03 11:44:28 -0600

    js connecting issue on server

--- ec2.md
+++ ec2.md
@@ -1,3 +1,5 @@
 ## Paste your link below
 
 https://trello.com/b/05lrGcAj/dualduo-qa-testing
+
+http://3.16.157.68/
--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/css', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '/public/styles.css'))
-})
-
 app.get('/js', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '/public/main.js'))
+})
+
+app.get('/css', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '/public/styles.css'))
 })
 
 
